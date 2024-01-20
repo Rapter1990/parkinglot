245d960266044a7740b4dbcfdd289504e0b3d775
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-20 21:36:44 +0300

    Task 6 : Devise a template for Exception Handling to define GlobalExceptionHandler and Define ErrorResponse

---
+++ src/main/java/com/project/parkinglot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,118 @@
+package com.project.parkinglot.exception;
+
+import com.project.parkinglot.payload.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+                                                                     HttpHeaders headers,
+                                                                     HttpStatusCode status,
+                                                                     WebRequest request) {
+        log.error(ex.getMessage(), ex);
+
+        List<String> details = new ArrayList<>();
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+
+        details.add(builder.toString());
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorDetails(details)
+                .message("Invalid JSON")
+                .statusCode(status.value())
+                .status(HttpStatus.valueOf(status.value()))
+                .build();
+
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        log.error(ex.getMessage(), ex);
+
+
+        List<String> details = new ArrayList<>();
+        details.add(ex.getMessage());
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorDetails(details)
+                .message("Malformed JSON request")
+                .statusCode(status.value())
+                .status(HttpStatus.valueOf(status.value()))
+                .build();
+
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        log.error(ex.getMessage(), ex);
+
+        List<String> details = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getObjectName() + " : " + error.getDefaultMessage())
+                .toList();
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorDetails(details)
+                .message("Validation Errors")
+                .statusCode(status.value())
+                .status(HttpStatus.valueOf(status.value()))
+                .build();
+
+        return ResponseEntity.status(status).body(errorResponse);
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                                          HttpHeaders headers,
+                                                                          HttpStatusCode status,
+                                                                          WebRequest request) {
+
+        log.error(ex.getMessage(), ex);
+
+        List<String> details = new ArrayList<>();
+        details.add(ex.getParameterName() + " parameter is missing");
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorDetails(details)
+                .message("Validation Errors")
+                .statusCode(status.value())
+                .status(HttpStatus.valueOf(status.value()))
+                .build();
+
+        return ResponseEntity.status(status).body(errorResponse);
+
+    }
+
+
+}
---
+++ src/main/java/com/project/parkinglot/payload/response/ErrorResponse.java
@@ -0,0 +1,32 @@
+package com.project.parkinglot.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    private HttpStatus status;
+
+    private Integer statusCode;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<String> errorDetails;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+}
