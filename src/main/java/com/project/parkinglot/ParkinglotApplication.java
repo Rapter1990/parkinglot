9674149f4db205163fba0a8912b5154b556f36f2
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-20 20:40:02 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/project/parkinglot/ParkinglotApplication.java
@@ -0,0 +1,13 @@
+package com.project.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/parkinglot/ParkinglotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
