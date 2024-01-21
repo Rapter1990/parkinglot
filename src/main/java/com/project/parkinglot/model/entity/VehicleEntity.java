4e385c07d3dbcddb108f37db9daa99bdfee1e939
moayd <m.o.aydogdu@outlook.com>
2024-01-21 05:53:44 +0300

    REFACTOR VehicleEntity.java -> BugFix in @Table Annotations name field.

--- src/main/java/com/project/parkinglot/model/entity/VehicleEntity.java
+++ src/main/java/com/project/parkinglot/model/entity/VehicleEntity.java
@@ -1,9 +1,20 @@
 package com.project.parkinglot.model.entity;
 
-import com.project.parkinglot.common.model.BaseEntity;
+import com.project.parkinglot.common.model.entity.BaseEntity;
 import com.project.parkinglot.model.enums.VehicleType;
 import com.project.parkinglot.security.model.entity.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,7 +31,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "PRICE_LIST")
+@Table(name = "VEHICLE")
 public class VehicleEntity extends BaseEntity {
 
     @Id
