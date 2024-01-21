1581a13f77799a6152e152cc6653ef1b08a892e9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-21 14:46:01 +0300

    Task 19 : Define LoginRequest, LogoutRequest, SignupRequest and TokenRefreshRequest

---
+++ src/main/java/com/project/parkinglot/payload/request/auth/TokenRefreshRequest.java
@@ -0,0 +1,18 @@
+package com.project.parkinglot.payload.request.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenRefreshRequest {
+
+    @NotBlank
+    private String refreshToken;
+
+}
