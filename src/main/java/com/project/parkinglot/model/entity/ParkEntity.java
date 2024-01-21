a6ff9b576a8362577e5eb115b3027050a1486514
moayd <m.o.aydogdu@outlook.com>
2024-01-21 03:42:59 +0300

    CREATE ParkEntity.java

---
+++ src/main/java/com/project/parkinglot/model/entity/ParkEntity.java
@@ -0,0 +1,62 @@
+package com.project.parkinglot.model.entity;
+
+import com.project.parkinglot.common.model.BaseEntity;
+import com.project.parkinglot.model.enums.ParkStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PARK")
+public class ParkEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "CHECK_IN")
+    private LocalDateTime checkIn;
+
+    @Column(name = "CHECK_OUT")
+    private LocalDateTime checkOut;
+
+    @Column(name = "TOTAL_COST")
+    private BigDecimal totalCost;
+
+    @ManyToOne(
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(
+            name = "PARKING_AREA_ID",
+            referencedColumnName = "ID"
+    )
+    private ParkingAreaEntity parkingAreaEntity;
+
+    @ManyToOne(
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(
+            name = "VEHICLE_ID",
+            referencedColumnName = "ID"
+    )
+    private VehicleEntity vehicleEntity;
+
+    @Column(name = "STATUS")
+    @Enumerated(EnumType.STRING)
+    private ParkStatus status;
+
+}
