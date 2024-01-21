69bdbfdd35cb72312af83ebb080fb72228837c61
moayd <m.o.aydogdu@outlook.com>
2024-01-21 05:52:57 +0300

    CREATE PriceRepository.java

---
+++ src/main/java/com/project/parkinglot/repository/PriceRepository.java
@@ -0,0 +1,9 @@
+package com.project.parkinglot.repository;
+
+import com.project.parkinglot.model.entity.PriceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceRepository extends JpaRepository<PriceEntity, String> {
+}
