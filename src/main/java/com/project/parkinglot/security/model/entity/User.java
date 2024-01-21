32c17d7a93467b84fb0f5f6c8be64569a9c0eac4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-21 16:57:32 +0300

    Task 27 : Define TokenClaims and Define its methods in User

--- src/main/java/com/project/parkinglot/security/model/entity/User.java
+++ src/main/java/com/project/parkinglot/security/model/entity/User.java
@@ -2,13 +2,16 @@ package com.project.parkinglot.security.model.entity;
 
 import com.project.parkinglot.model.entity.VehicleEntity;
 import com.project.parkinglot.security.model.enums.Role;
+import com.project.parkinglot.security.model.enums.TokenClaims;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Data
@@ -39,5 +42,15 @@ public class User {
             mappedBy = "user"
     )
     private List<VehicleEntity> vehicles;
+
+    public Map<String, Object> getClaims() {
+        final Map<String, Object> claims = new HashMap<>();
+        claims.put(TokenClaims.ID.getValue(), this.id);
+        claims.put(TokenClaims.USERNAME.getValue(), this.username);
+        claims.put(TokenClaims.ROLES.getValue(), List.of(this.role));
+        claims.put(TokenClaims.USER_FULL_NAME.getValue(), this.fullName);
+        claims.put(TokenClaims.EMAIL.getValue(), this.email);
+        return claims;
+    }
 
 }
