1a9794c75085fd886e1c45180933d840522c0d3a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-21 17:00:42 +0300

    Task 30 : Implement AuthServiceImpl, RefreshTokenServiceImpl and UserServiceImpl

---
+++ src/main/java/com/project/parkinglot/service/auth/impl/AuthServiceImpl.java
@@ -0,0 +1,119 @@
+package com.project.parkinglot.service.auth.impl;
+
+import com.project.parkinglot.exception.user.EmailAlreadyExistsException;
+import com.project.parkinglot.exception.user.RefreshTokenNotFoundException;
+import com.project.parkinglot.exception.user.UserNotFoundException;
+import com.project.parkinglot.payload.request.auth.LoginRequest;
+import com.project.parkinglot.payload.request.auth.SignupRequest;
+import com.project.parkinglot.payload.request.auth.TokenRefreshRequest;
+import com.project.parkinglot.payload.response.auth.JWTResponse;
+import com.project.parkinglot.payload.response.auth.TokenRefreshResponse;
+import com.project.parkinglot.repository.UserRepository;
+import com.project.parkinglot.security.CustomUserDetails;
+import com.project.parkinglot.security.jwt.JwtUtils;
+import com.project.parkinglot.security.model.entity.RefreshToken;
+import com.project.parkinglot.security.model.entity.User;
+import com.project.parkinglot.service.auth.AuthService;
+import com.project.parkinglot.service.auth.RefreshTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final RefreshTokenService refreshTokenService;
+
+    private final JwtUtils jwtUtils;
+
+
+    @Override
+    public String register(SignupRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistsException(request.getEmail());
+        }
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .fullName(request.getFullName())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        userRepository.save(user);
+
+        return "Success";
+    }
+
+
+    @Override
+    public JWTResponse login(LoginRequest request) {
+
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+
+        Authentication auth = authenticationManager.authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwtToken = jwtUtils.generateJwtToken(auth);
+
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(UserNotFoundException::new);
+
+        return JWTResponse.builder()
+                .email(request.getEmail())
+                .token(jwtToken)
+                .refreshToken(refreshTokenService.createRefreshToken(user))
+                .build();
+    }
+
+
+    @Override
+    public TokenRefreshResponse refreshToken(TokenRefreshRequest request) {
+
+        RefreshToken refreshToken = refreshTokenService.findByToken(request.getRefreshToken())
+                .orElseThrow(RefreshTokenNotFoundException::new);
+
+
+        if (!refreshTokenService.isRefreshExpired(refreshToken)) {
+            CustomUserDetails customUserDetails = new CustomUserDetails(refreshToken.getUser());
+            String newToken = jwtUtils.generateJwtToken(customUserDetails);
+
+            return TokenRefreshResponse.builder()
+                    .accessToken(newToken)
+                    .refreshToken(refreshToken.getToken())
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Override
+    public String logout(String token) {
+
+        String authToken = jwtUtils.extractTokenFromHeader(token);
+
+        if (jwtUtils.validateJwtToken(authToken)) {
+            Long id = jwtUtils.getIdFromToken(authToken);
+
+            refreshTokenService.deleteByUserId(id);
+
+            return "Success";
+        }
+
+        return "Failed";
+
+    }
+
+}
