3f0281d7c37df0e97ed61dd86318ac4d55c8b652
moayd <m.o.aydogdu@outlook.com>
2024-01-21 03:47:40 +0300

    REFACTOR Role.java -> Add @Getter and @RequiredArgsConstructor

--- src/main/java/com/project/parkinglot/entity/enums/Role.java
+++
@@ -1,8 +0,0 @@
-package com.project.parkinglot.entity.enums;
-
-public enum Role {
-
-    ROLE_DRIVER,
-    ROLE_ADMIN
-
-}
---
+++ src/main/java/com/project/parkinglot/security/model/enums/Role.java
@@ -0,0 +1,11 @@
+package com.project.parkinglot.security.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    ROLE_DRIVER,
+    ROLE_ADMIN
+}
