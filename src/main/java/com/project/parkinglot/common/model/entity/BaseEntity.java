ae215d1bfc6347cecc1f178e34d3163090eafce3
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-21 23:32:45 +0300

    Task 38 : Add getUsernameFromAuthentication method process for createdUser and updatedUser in BaseEntity

--- src/main/java/com/project/parkinglot/common/model/entity/BaseEntity.java
+++ src/main/java/com/project/parkinglot/common/model/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.project.parkinglot.common.model.entity;
 
+import com.project.parkinglot.security.CustomUserDetails;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
@@ -9,6 +10,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
 
@@ -34,14 +37,22 @@ public abstract class BaseEntity {
 
     @PrePersist
     public void prePersist() {
-        this.createdUser = "createdUser";
+        this.createdUser = getUsernameFromAuthentication();
         this.createdAt = LocalDateTime.now();
     }
 
     @PreUpdate
     public void preUpdate() {
-        this.updatedUser = "updatedUser";
+        this.updatedUser = getUsernameFromAuthentication();
         this.updatedAt = LocalDateTime.now();
+    }
+
+    private String getUsernameFromAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
+            return userDetails.getUsername();
+        }
+        return "anonymousUser";
     }
 
 }
