f4b7394e5c0456ca3d4daedff604a86d50b8e642
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-21 17:02:20 +0300

    Task 32 : Define AbstractTestContainerConfiguration, BaseControllerTest and BaseServiceTest

---
+++ src/test/java/com/project/parkinglot/base/AbstractTestContainerConfiguration.java
@@ -0,0 +1,27 @@
+package com.project.parkinglot.base;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractTestContainerConfiguration {
+
+    static MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0.33");
+
+    @BeforeAll
+    static void beforeAll() {
+        MYSQL_CONTAINER.withReuse(true);
+        MYSQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    private static void overrideProps(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        dynamicPropertyRegistry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+    }
+
+}
