e1274468e8458c62068f3ed707b5cc264479ecbb
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-01-28 00:15:48 +0300

    Task 47 : Define VehicleNotFoundException

--- src/main/java/com/project/parkinglot/exception/vehicle/VehicleNotFoundException.java
+++ src/main/java/com/project/parkinglot/exception/vehicle/VehicleNotFoundException.java
@@ -1,5 +1,26 @@
 package com.project.parkinglot.exception.vehicle;
 
-public class VehicleNotFoundException {
+import com.project.parkinglot.exception.NotFoundException;
+
+import java.io.Serial;
+
+public class VehicleNotFoundException extends NotFoundException {
+
+    @Serial
+    private static final long serialVersionUID = 5431951439973055123L;
+
+    private static final String DEFAULT_MESSAGE =
+            "The specified vehicle is not found";
+
+    private static final String MESSAGE_TEMPLATE =
+            "No vehicle was found with ID: ";
+
+    public VehicleNotFoundException(String id) {
+        super(MESSAGE_TEMPLATE.concat(id));
+    }
+
+    public VehicleNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
 
 }
