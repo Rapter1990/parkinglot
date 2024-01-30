443ce3d4742848b6ebf2287b2eff8ceb195a087c
moayd <m.o.aydogdu@outlook.com>
2024-01-30 21:03:29 +0300

    REFACTOR ParkingArea.java -> Remove relative fields in domain model.

--- src/main/java/com/project/parkinglot/model/ParkingArea.java
+++ src/main/java/com/project/parkinglot/model/ParkingArea.java
@@ -1,10 +1,12 @@
 package com.project.parkinglot.model;
 
 import com.project.parkinglot.common.model.BaseDomainModel;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -19,8 +21,5 @@ public class ParkingArea extends BaseDomainModel {
     private String location;
     private Integer capacity;
     private String city;
-    private PriceList priceList;
-    private List<Park> parkList;
-    private List<DailyIncome> dailyIncomeList;
 
 }
