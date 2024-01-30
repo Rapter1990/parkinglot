46ba71365eba0a0c9b5dd422679bec15315c5f53
moayd <m.o.aydogdu@outlook.com>
2024-01-30 21:03:56 +0300

    REFACTOR ParkingAreaController.java -> Add getParkingAreaById method.

--- src/main/java/com/project/parkinglot/controller/ParkingAreaController.java
+++ src/main/java/com/project/parkinglot/controller/ParkingAreaController.java
@@ -1,7 +1,13 @@
 package com.project.parkinglot.controller;
 
+import com.project.parkinglot.model.ParkingArea;
+import com.project.parkinglot.service.parking_area.ParkingAreaService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Validated
 public class ParkingAreaController {
+
+    private final ParkingAreaService parkingAreaService;
+
+    @GetMapping("/{parking-area-id}")
+    public ResponseEntity<ParkingArea> getParkingAreaById(
+        @PathVariable("parking-area-id") @UUID final String parkingAreaId
+    ) {
+        final ParkingArea parkingArea = parkingAreaService
+                .getParkingAreaById(parkingAreaId);
+
+        return ResponseEntity.ok(parkingArea);
+    }
 
 }
