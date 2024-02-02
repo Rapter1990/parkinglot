93f5679684e1c64a2633090a187260880349fe05
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-02-02 17:30:09 +0100

    Create ParkingAreaCreateServiceImplTest.java

---
+++ src/test/java/com/project/parkinglot/service/parking_area/impl/ParkingAreaCreateServiceImplTest.java
@@ -0,0 +1,96 @@
+package com.project.parkinglot.service.parking_area.impl;
+
+import com.project.parkinglot.base.BaseServiceTest;
+import com.project.parkinglot.builder.ParkingAreaCreateRequestBuilder;
+import com.project.parkinglot.exception.parkingarea.ParkingAreAlreadyExistException;
+import com.project.parkinglot.model.ParkingArea;
+import com.project.parkinglot.model.dto.request.parking_area.ParkingAreaCreateRequest;
+import com.project.parkinglot.model.entity.ParkingAreaEntity;
+import com.project.parkinglot.repository.ParkingAreaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class ParkingAreaCreateServiceImplTest extends BaseServiceTest {
+
+
+    @InjectMocks
+    private ParkingAreaCreateServiceImpl parkingAreaCreateService;
+
+    @Mock
+    private ParkingAreaRepository parkingAreaRepository;
+
+    @Test
+    public void givenValidParkingAreaCreateRequest_whenCreateParkingArea_thenReturnParkingAreaDomainModel() {
+        // Given
+        final ParkingAreaCreateRequest mockValidParkingAreaCreateRequest = new ParkingAreaCreateRequestBuilder()
+                .withValidFields()
+                .build();
+
+        // When
+        Mockito.when(parkingAreaRepository.existsParkingAreaEntitiesByNameAndLocation(
+                Mockito.anyString(),
+                Mockito.anyString())
+        ).thenReturn(Boolean.FALSE);
+
+        // Then
+        ParkingArea response = parkingAreaCreateService
+                .createParkingArea(mockValidParkingAreaCreateRequest);
+
+        Assertions.assertEquals(
+                mockValidParkingAreaCreateRequest.getName(),
+                response.getName()
+        );
+
+        Assertions.assertEquals(
+                mockValidParkingAreaCreateRequest.getLocation(),
+                response.getLocation()
+        );
+
+        // Verify
+        Mockito.verify(
+                parkingAreaRepository,
+                Mockito.times(1)
+        ).save(Mockito.any(ParkingAreaEntity.class));
+
+        Mockito.verify(
+                parkingAreaRepository,
+                Mockito.times(1)
+        ).existsParkingAreaEntitiesByNameAndLocation(Mockito.anyString(), Mockito.anyString());
+
+    }
+
+    @Test
+    public void givenNotValidParkingAreaCreateRequest_whenCreateParkingArea_thenThrowsException() {
+        // Given
+        final ParkingAreaCreateRequest mockNotValidParkingAreaCreateRequest = new ParkingAreaCreateRequestBuilder()
+                .withValidFields()
+                .build();
+
+        // When
+        Mockito.when(parkingAreaRepository.existsParkingAreaEntitiesByNameAndLocation(
+                Mockito.anyString(),
+                Mockito.anyString())
+        ).thenReturn(Boolean.TRUE);
+
+        Assertions.assertThrowsExactly(
+                ParkingAreAlreadyExistException.class,
+                () -> parkingAreaCreateService.createParkingArea(mockNotValidParkingAreaCreateRequest)
+        );
+
+        // Verify
+        Mockito.verify(
+                parkingAreaRepository,
+                Mockito.times(1)
+        ).existsParkingAreaEntitiesByNameAndLocation(Mockito.anyString(), Mockito.anyString());
+
+        Mockito.verify(
+                parkingAreaRepository,
+                Mockito.times(0)
+        ).save(Mockito.any(ParkingAreaEntity.class));
+
+    }
+
+}
