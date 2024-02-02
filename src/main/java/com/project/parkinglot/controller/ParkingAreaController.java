ab62f8cfa7a0200f99ca842c202b208475f5e761
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-02-02 17:36:42 +0100

    Create ParkingAreaController.java -> added createParkingArea post method

--- src/main/java/com/project/parkinglot/controller/ParkingAreaController.java
+++ src/main/java/com/project/parkinglot/controller/ParkingAreaController.java
@@ -1,14 +1,35 @@
 package com.project.parkinglot.controller;
 
+import com.project.parkinglot.model.ParkingArea;
+import com.project.parkinglot.model.dto.request.parking_area.ParkingAreaCreateRequest;
+import com.project.parkinglot.service.parking_area.ParkingAreaCreateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/parking-area")
 @RequiredArgsConstructor
 @Validated
 public class ParkingAreaController {
+
+    private final ParkingAreaCreateService parkingAreaCreateService;
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    public ResponseEntity<String> createParkingArea(
+            @RequestBody final ParkingAreaCreateRequest parkingAreaCreateRequest
+    ) {
+        final ParkingArea parkingArea = parkingAreaCreateService
+                .createParkingArea(parkingAreaCreateRequest);
+
+        return ResponseEntity.ok(parkingArea.getId());
+    }
 
 }
