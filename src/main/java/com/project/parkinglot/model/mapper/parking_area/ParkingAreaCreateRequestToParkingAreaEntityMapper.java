055b10e9de1be66407f68737e7f1350e343896fc
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-02-03 12:24:26 +0100

    Created ParkingAreaCreateRequestToParkingAreaEntityMapper.java

---
+++ src/main/java/com/project/parkinglot/model/mapper/parking_area/ParkingAreaCreateRequestToParkingAreaEntityMapper.java
@@ -0,0 +1,26 @@
+package com.project.parkinglot.model.mapper.parking_area;
+
+import com.project.parkinglot.model.dto.request.parking_area.ParkingAreaCreateRequest;
+import com.project.parkinglot.model.entity.ParkingAreaEntity;
+import com.project.parkinglot.model.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ParkingAreaCreateRequestToParkingAreaEntityMapper extends BaseMapper<ParkingAreaCreateRequest, ParkingAreaEntity> {
+
+    @Override
+    // @Mapping(source = "priceListRequest.name", target = "priceListEntity.name")
+    @Mapping(source = "priceListRequest.prices", target = "priceListEntity.priceEntities")
+    ParkingAreaEntity map(ParkingAreaCreateRequest source);
+
+    /**
+     * Initializes the mapper.
+     *
+     * @return the initialized mapper object.
+     */
+    static ParkingAreaCreateRequestToParkingAreaEntityMapper initialize() {
+        return Mappers.getMapper(ParkingAreaCreateRequestToParkingAreaEntityMapper.class);
+    }
+}
