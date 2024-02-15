5d372ff8325a03fe3e65bffc02071fbbb163791f
Harun170518513 <yusufeksioglu53@gmail.com>
2024-02-15 23:12:00 +0300

    unit ve integration testler yapıldı

--- src/main/java/com/project/parkinglot/service/parking_area/impl/ParkingAreaUpdateServiceImpl.java
+++ src/main/java/com/project/parkinglot/service/parking_area/impl/ParkingAreaUpdateServiceImpl.java
@@ -16,9 +16,6 @@ import org.springframework.stereotype.Service;
 public class ParkingAreaUpdateServiceImpl implements ParkingAreaUpdateService {
     private final ParkingAreaRepository parkingAreaRepository;
 
-    private final ParkingAreaDomainModelToParkingAreaEntityMapper parkingAreaDomainModelToParkingAreaEntityMapper =
-            ParkingAreaDomainModelToParkingAreaEntityMapper.initialize();
-
     private final ParkingAreaEntityToParkingAreaDomainModel parkingAreaEntityToParkingAreaDomainModel =
             ParkingAreaEntityToParkingAreaDomainModel.initialize();
 
--- src/test/java/com/project/parkinglot/controller/ParkingAreaControllerTest.java
+++ src/test/java/com/project/parkinglot/controller/ParkingAreaControllerTest.java
@@ -2,15 +2,21 @@ package com.project.parkinglot.controller;
 
 import com.project.parkinglot.base.BaseControllerTest;
 import com.project.parkinglot.builder.ParkingAreaCreateRequestBuilder;
+import com.project.parkinglot.builder.ParkingAreaEntityBuilder;
+import com.project.parkinglot.builder.ParkingAreaUpdateRequestBuilder;
 import com.project.parkinglot.exception.parkingarea.ParkingAreaNotFoundException;
 import com.project.parkinglot.model.ParkingArea;
+import com.project.parkinglot.model.dto.request.parkingArea.ParkingAreaUpdateRequest;
 import com.project.parkinglot.model.dto.request.parking_area.ParkingAreaCreateRequest;
 import com.project.parkinglot.model.entity.ParkingAreaEntity;
 import com.project.parkinglot.model.mapper.parking_area.ParkingAreaCreateRequestToParkingAreaEntityMapper;
 import com.project.parkinglot.model.mapper.parking_area.ParkingAreaEntityToParkingAreaDomainModelMapper;
+import com.project.parkinglot.service.parking_area.ParkingAreaUpdateService;
 import com.project.parkinglot.service.parking_area.impl.ParkingAreaCreateServiceImpl;
 import com.project.parkinglot.service.parking_area.impl.ParkingAreaDeleteServiceImpl;
+import com.project.parkinglot.service.parking_area.impl.ParkingAreaUpdateServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
@@ -19,6 +25,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
@@ -32,6 +39,9 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
     @MockBean
     private ParkingAreaDeleteServiceImpl parkingAreaDeleteService;
 
+    @MockBean
+    private ParkingAreaUpdateServiceImpl parkingAreaUpdateService;
+
     private final ParkingAreaCreateRequestToParkingAreaEntityMapper parkingAreaCreateRequestToParkingAreaEntityMapper =
             ParkingAreaCreateRequestToParkingAreaEntityMapper.initialize();
 
@@ -39,7 +49,7 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
             ParkingAreaEntityToParkingAreaDomainModelMapper.initialize();
 
     @Test
-    public void givenValidParkingAreaCreateRequest_whenParkingAreaCreated_thenReturnCustomResponse() throws Exception {
+     void givenValidParkingAreaCreateRequest_whenParkingAreaCreated_thenReturnCustomResponse() throws Exception {
 
         // Given
         final String mockParkingAreaId = UUID.randomUUID().toString();
@@ -81,7 +91,7 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void givenValidParkingAreaCreateRequest_whenUserUnauthorized_thenReturnForbidden() throws Exception {
+     void givenValidParkingAreaCreateRequest_whenUserUnauthorized_thenReturnForbidden() throws Exception {
 
         // Given
         final ParkingAreaCreateRequest mockParkingAreaCreateRequest = new ParkingAreaCreateRequestBuilder()
@@ -215,7 +225,7 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void givenValidParkingAreaId_whenParkingAreaDeleted_thenReturnCustomResponse() throws Exception {
+     void givenValidParkingAreaId_whenParkingAreaDeleted_thenReturnCustomResponse() throws Exception {
 
         // Given
         final String mockParkingAreaId = UUID.randomUUID().toString();
@@ -243,7 +253,7 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void givenInvalidParkingAreaId_whenParkingAreaDeleted_thenReturnNotFound() throws Exception {
+     void givenInvalidParkingAreaId_whenParkingAreaDeleted_thenReturnNotFound() throws Exception {
 
         // Given
         String invalidParkingAreaId = UUID.randomUUID().toString();
@@ -269,7 +279,7 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void givenValidParkingAreaId_whenUserUnauthorized_thenReturnForbidden() throws Exception {
+     void givenValidParkingAreaId_whenUserUnauthorized_thenReturnForbidden() throws Exception {
 
         // Given
         final String mockParkingAreaId = UUID.randomUUID().toString();
@@ -293,6 +303,132 @@ public class ParkingAreaControllerTest extends BaseControllerTest {
                 .deleteParkingAreaById(mockParkingAreaId);
 
     }
+
+    @Test
+    void givenValidParkingAreaUpdateRequest_whenParkingAreaUpdated_thenReturnCustomResponse() throws Exception{
+
+        //Given
+
+        final String mockUpdateEntityId = UUID.randomUUID().toString();
+
+        final ParkingAreaUpdateRequest mockParkingAreaUpdateRequest = new ParkingAreaUpdateRequestBuilder()
+                .withValidField()
+                .build();
+
+        final ParkingAreaEntity mockParkingAreaEntity = new ParkingAreaEntityBuilder()
+                .withValidFields()
+                .build();
+
+        mockParkingAreaEntity.setId(mockUpdateEntityId);
+        mockParkingAreaEntity.setCapacity(mockParkingAreaUpdateRequest.getCapacity());
+
+
+        final ParkingArea mockParkingAreaDomainModel = parkingAreaEntityToParkingAreaDomainModelMapper
+                .map(mockParkingAreaEntity);
+
+        //when
+        Mockito.when(
+                parkingAreaUpdateService.parkingAreaUpdateByCapacity(
+                        mockUpdateEntityId,
+                        mockParkingAreaUpdateRequest
+                        )
+        ).thenReturn(mockParkingAreaDomainModel);
+
+
+        //then
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .put("api/v1/parking-area/{id}",mockUpdateEntityId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockParkingAreaUpdateRequest))
+                        .header(HttpHeaders.AUTHORIZATION,mockAdminToken)
+        )
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response").value(mockParkingAreaDomainModel.getId()))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(true))
+        .andExpect(MockMvcResultMatchers.jsonPath("httpStatus").value("ok"));
+
+        //verify
+        Mockito.verify(parkingAreaUpdateService,Mockito.times(1))
+                .parkingAreaUpdateByCapacity(Mockito.anyString(),Mockito.any(ParkingAreaUpdateRequest.class));
+    }
+
+    @Test
+    void givenInvalidParkingAreaUpdateRequest_whenParkingAreaCapacityIsNull_thenReturnBadRequest() throws Exception {
+
+        //Given
+        final String mockParkingAreaId = UUID.randomUUID().toString();
+
+        final ParkingAreaUpdateRequest mockParkingAreaUpdateRequest = new ParkingAreaUpdateRequestBuilder()
+                .withValidField()
+                .withCapacity(null)
+                .build();
+
+        final ParkingArea mockParkingArea = ParkingArea.builder()
+                .id(mockParkingAreaId)
+                .capacity(mockParkingAreaUpdateRequest.getCapacity())
+                .build();
+
+        //when
+        Mockito.when(
+                parkingAreaUpdateService.parkingAreaUpdateByCapacity(
+                        mockParkingAreaId,
+                        mockParkingAreaUpdateRequest
+                )
+        ).thenThrow(new RuntimeException());
+
+
+        //then
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put("api/v1/parking-area/{id}",mockParkingAreaId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(mockParkingAreaUpdateRequest))
+                                .header(HttpHeaders.AUTHORIZATION,mockAdminToken)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+
+
+        //verify
+        Mockito.verify(parkingAreaUpdateService,Mockito.times(1))
+                .parkingAreaUpdateByCapacity(mockParkingAreaId,mockParkingAreaUpdateRequest);
+
+    }
+
+    @Test
+    void givenValidParkingAreaUpdateRequest_whenUserUnauthorized_thenReturnForbidden() throws Exception{
+
+        //Given
+        final ParkingAreaUpdateRequest mockUpdateRequest = new ParkingAreaUpdateRequestBuilder()
+                .withValidField()
+                .build();
+
+        final String mockParkingAreaId = UUID.randomUUID().toString();
+
+        //when
+        Mockito.doNothing().when(parkingAreaUpdateService.parkingAreaUpdateByCapacity(mockParkingAreaId,mockUpdateRequest));
+
+        //then
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .put("api/v1/parking-area/{id}",mockParkingAreaId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION,mockUserToken)
+        )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+
+        //Verify
+        Mockito.verify(parkingAreaUpdateService,Mockito.times(0))
+                .parkingAreaUpdateByCapacity(mockParkingAreaId,mockUpdateRequest);
+    }
+
+
+
+
 
 }
 
