96be0f8ffc5f121b0398b0c7056254be6702b7bd
Harun170518513 <123752776+Harun170518513@users.noreply.github.com>
2024-03-02 15:56:44 +0300

    Generate Service interface and create a class that implement this interface

---
+++ src/main/java/com/project/parkinglot/service/vehicle/VehicleService.java
@@ -0,0 +1,12 @@
+package com.project.parkinglot.service.vehicle;
+
+import com.project.parkinglot.model.Vehicle;
+import com.project.parkinglot.model.dto.request.Vehicle.VehicleRequest;
+
+public interface VehicleService {
+
+    Vehicle assignVehicleToUser (
+            String id,
+            VehicleRequest vehicleRequest
+    );
+}
