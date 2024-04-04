c7b6344d69da9f2b4a1c55de16331348cf7e430d
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-04 20:19:21 +0200

    Refactor ParkControllerTest.java --> created test scenarios for checkout endpoint

--- src/test/java/com/project/parkinglot/controller/ParkControllerTest.java
+++ src/test/java/com/project/parkinglot/controller/ParkControllerTest.java
@@ -4,9 +4,12 @@ import com.project.parkinglot.base.BaseControllerTest;
 import com.project.parkinglot.builder.VehicleBuilder;
 import com.project.parkinglot.model.Vehicle;
 import com.project.parkinglot.model.dto.request.park.ParkCheckInRequest;
+import com.project.parkinglot.model.dto.request.park.ParkCheckOutRequest;
 import com.project.parkinglot.model.dto.request.vehicle.VehicleRequest;
-import com.project.parkinglot.model.dto.response.ParkCheckInResponse;
-import com.project.parkinglot.model.dto.response.VehicleCheckInResponse;
+import com.project.parkinglot.model.dto.response.park.ParkCheckInResponse;
+import com.project.parkinglot.model.dto.response.park.ParkCheckOutResponse;
+import com.project.parkinglot.model.dto.response.vehicle.VehicleCheckInResponse;
+import com.project.parkinglot.model.dto.response.vehicle.VehicleCheckOutResponse;
 import com.project.parkinglot.model.enums.ParkStatus;
 import com.project.parkinglot.service.park.ParkService;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -133,7 +137,7 @@ class ParkControllerTest extends BaseControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized());
 
         // Verify
-        verify(parkService,Mockito.never()).checkIn(mockUserId, parkCheckInRequest);
+        verify(parkService, Mockito.never()).checkIn(mockUserId, parkCheckInRequest);
 
     }
 
@@ -168,9 +172,152 @@ class ParkControllerTest extends BaseControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
 
         // Verify
-        verify(parkService,Mockito.never()).checkIn(mockUserId, parkCheckInRequest);
+        verify(parkService, Mockito.never()).checkIn(mockUserId, parkCheckInRequest);
 
     }
 
+    @Test
+    void givenUserIdAndParkCheckOutRequest_WhenCheckOut_ThenReturnParkCheckOutResponse() throws Exception {
+        // Given
+        final String mockUserId = UUID.randomUUID().toString();
+        final String mockParkingAreaId = "ParkingArea123";
+        final String mockParkingAreaName = "ParkingArea";
 
+        final Vehicle vehicle = new VehicleBuilder()
+                .withValidFields()
+                .build();
+
+        final VehicleRequest vehicleRequest = VehicleRequest.builder()
+                .vehicleType(vehicle.getVehicleType())
+                .licensePlate(vehicle.getLicensePlate())
+                .build();
+
+        final ParkCheckOutRequest parkCheckOutRequest = ParkCheckOutRequest.builder()
+                .parkingAreaId(mockParkingAreaId)
+                .vehicleRequest(vehicleRequest)
+                .build();
+
+        final ParkCheckOutResponse parkCheckOutResponse = ParkCheckOutResponse.builder()
+                .parkingAreaId(mockParkingAreaId)
+                .parkingAreaName(mockParkingAreaName)
+                .vehicleCheckOutResponse(VehicleCheckOutResponse.builder()
+                        .vehicleType(vehicleRequest.getVehicleType())
+                        .licensePlate(vehicleRequest.getLicensePlate())
+                        .build())
+                .parkStatus(ParkStatus.EMPTY)
+                .checkOut(LocalDateTime.now())
+                .totalCost(BigDecimal.TEN)
+                .build();
+
+        // When
+        Mockito.when(parkService.checkOut(anyString(), any(ParkCheckOutRequest.class)))
+                .thenReturn(parkCheckOutResponse);
+
+        // Then
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/api/v1/parks/userId/{userId}/check-out", mockUserId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(parkCheckOutRequest))
+                                .header(HttpHeaders.AUTHORIZATION, mockUserToken)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.parkingAreaId")
+                        .value(parkCheckOutResponse.getParkingAreaId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.parkingAreaName")
+                        .value(parkCheckOutResponse.getParkingAreaName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.parkStatus")
+                        .value(parkCheckOutResponse.getParkStatus().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.vehicleCheckOutResponse.licensePlate")
+                        .value(parkCheckOutResponse.getVehicleCheckOutResponse().getLicensePlate()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.vehicleCheckOutResponse.vehicleType")
+                        .value(parkCheckOutResponse.getVehicleCheckOutResponse().getVehicleType().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.totalCost")
+                        .value(parkCheckOutResponse.getTotalCost()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.httpStatus").value("OK"));
+
+        // Verify
+        Mockito.verify(parkService, Mockito.times(1))
+                .checkOut(anyString(), any(ParkCheckOutRequest.class));
+    }
+
+    @Test
+    void givenUserIdAndParkCheckOutResponse_whenUserTokenNotAvailable_thenReturnUnauthorized() throws Exception {
+        // Given
+        final String mockUserId = UUID.randomUUID().toString();
+        final String mockParkingAreaId = "ParkingArea123";
+        final String mockParkingAreaName = "ParkingArea";
+
+        final Vehicle vehicle = new VehicleBuilder()
+                .withValidFields()
+                .build();
+
+        final VehicleRequest vehicleRequest = VehicleRequest.builder()
+                .vehicleType(vehicle.getVehicleType())
+                .licensePlate(vehicle.getLicensePlate())
+                .build();
+
+        final ParkCheckOutRequest parkCheckOutRequest = ParkCheckOutRequest.builder()
+                .parkingAreaId(mockParkingAreaId)
+                .vehicleRequest(vehicleRequest)
+                .build();
+
+        // When
+        Mockito.when(parkService.checkOut(anyString(), any(ParkCheckOutRequest.class)))
+                .thenReturn(null);
+
+        // Then
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/api/v1/parks/userId/{userId}/check-out", mockUserId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(parkCheckOutRequest))
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
+        // Verify
+        Mockito.verify(parkService, Mockito.never())
+                .checkOut(anyString(), any(ParkCheckOutRequest.class));
+    }
+
+    @Test
+    void givenUserIdAndParkCheckOutResponse_whenAdminTokenUsage_thenReturnForbidden() throws Exception {
+        // Given
+        final String mockUserId = UUID.randomUUID().toString();
+        final String mockParkingAreaId = "ParkingArea123";
+        final String mockParkingAreaName = "ParkingArea";
+
+        final Vehicle vehicle = new VehicleBuilder()
+                .withValidFields()
+                .build();
+
+        final VehicleRequest vehicleRequest = VehicleRequest.builder()
+                .vehicleType(vehicle.getVehicleType())
+                .licensePlate(vehicle.getLicensePlate())
+                .build();
+
+        final ParkCheckOutRequest parkCheckOutRequest = ParkCheckOutRequest.builder()
+                .parkingAreaId(mockParkingAreaId)
+                .vehicleRequest(vehicleRequest)
+                .build();
+
+        // Then
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/api/v1/parks/userId/{userId}/check-out", mockUserId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(parkCheckOutRequest))
+                                .header(HttpHeaders.AUTHORIZATION, mockAdminToken)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+
+        // Verify
+        Mockito.verify(parkService, Mockito.never())
+                .checkOut(anyString(), any(ParkCheckOutRequest.class));
+
+    }
 }
