14e744748094f10cb30f4c99637541f289cfab2b
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-04 20:15:28 +0200

    Refactor ParkService.java --> added checkOut method

--- src/main/java/com/project/parkinglot/service/park/ParkService.java
+++ src/main/java/com/project/parkinglot/service/park/ParkService.java
@@ -1,7 +1,9 @@
 package com.project.parkinglot.service.park;
 
 import com.project.parkinglot.model.dto.request.park.ParkCheckInRequest;
-import com.project.parkinglot.model.dto.response.ParkCheckInResponse;
+import com.project.parkinglot.model.dto.request.park.ParkCheckOutRequest;
+import com.project.parkinglot.model.dto.response.park.ParkCheckInResponse;
+import com.project.parkinglot.model.dto.response.park.ParkCheckOutResponse;
 import com.project.parkinglot.model.entity.ParkingAreaEntity;
 
 public interface ParkService {
@@ -9,5 +11,10 @@ public interface ParkService {
     ParkCheckInResponse checkIn(final String userId, final ParkCheckInRequest requestBody);
 
     Integer countCurrentParks(ParkingAreaEntity parkingAreaEntity);
+
+    ParkCheckOutResponse checkOut(
+            final String userId,
+            final ParkCheckOutRequest parkCheckOutRequest
+    );
 
 }
