5cc40e0de283d7f40f6bf971b92106c94d88282b
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-05 12:35:38 +0200

    Created ParkCheckOutResponse.java

---
+++ src/main/java/com/project/parkinglot/model/dto/response/park/ParkCheckOutResponse.java
@@ -0,0 +1,30 @@
+package com.project.parkinglot.model.dto.response.park;
+
+import com.project.parkinglot.common.model.BaseDomainModel;
+import com.project.parkinglot.model.dto.response.vehicle.VehicleCheckOutResponse;
+import com.project.parkinglot.model.enums.ParkStatus;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkCheckOutResponse extends BaseDomainModel {
+
+    private String parkingAreaId;
+    private String parkingAreaName;
+    private VehicleCheckOutResponse vehicleCheckOutResponse;
+    private ParkStatus parkStatus;
+    private LocalDateTime checkOut;
+    private BigDecimal totalCost;
+}
