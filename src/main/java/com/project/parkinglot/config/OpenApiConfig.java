d7197b146c2ca8deea9360b861465246978c1bc4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-04-13 13:51:39 +0300

    Revise OpenApiConfig for SecurityScheme

--- src/main/java/com/project/parkinglot/config/OpenApiConfig.java
+++ src/main/java/com/project/parkinglot/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.project.parkinglot.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -15,6 +18,14 @@ import io.swagger.v3.oas.annotations.info.Info;
                 title = "Parking Lot App API",
                 version = "1.0.0"
         )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
 
