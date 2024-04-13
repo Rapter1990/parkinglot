6f9fcdf9c318173e4c087ba4672c177de4c37ca9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-04-13 13:47:53 +0300

    Revise pom.xml for version and OpenApiConfig

--- src/main/java/com/project/parkinglot/config/OpenApiConfig.java
+++ src/main/java/com/project/parkinglot/config/OpenApiConfig.java
@@ -7,11 +7,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
-                        name = "Sercan Noyan Germiyanoğlu",
-                        url = "https://github.com/Rapter1990/bookdelivery/"
+                        name = "Sercan Noyan Germiyanoğlu | Mehmet Şeymus Yüzen | Harun Yusuf Ekşioğlu | Muhammet Oğuzhan Aydoğan",
+                        url = "https://github.com/Rapter1990/parkinglot/"
                 ),
                 description = "Case Study - Parking Lot " +
-                        "(Spring Boot, Spring Security , Mysql, JUnit, Integration Test, Docker, Test Container, AOP) ",
+                        "(Spring Boot, Spring Security , Mysql, JUnit, Integration Test, Docker, Test Container, AOP, Prometheus, Grafana) ",
                 title = "Parking Lot App API",
                 version = "1.0.0"
         )
