f7d6e5f1cd2b5aa386df5c1ec9e4e2bfe6eb0b83
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-18 11:42:30 +0200

    Created InvalidDateException.java

---
+++ src/main/java/com/project/parkinglot/exception/parkingarea/InvalidDateException.java
@@ -0,0 +1,22 @@
+package com.project.parkinglot.exception.parkingarea;
+
+import java.io.Serial;
+
+public class InvalidDateException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 3573563192487343800L;
+
+    private static final String DEFAULT_MESSAGE =
+            "Cannot be processed for a future date";
+
+    public InvalidDateException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public InvalidDateException(
+            final String message
+    ) {
+        super(DEFAULT_MESSAGE + " " + message);
+    }
+}
