d89b1bd9fd798a76cf8c82b4a4ce1bb65a4189dc
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-18 11:34:26 +0200

    Deleted DailyIncomeRepository.java

--- src/main/java/com/project/parkinglot/repository/DailyIncomeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.parkinglot.repository;
-
-import com.project.parkinglot.model.entity.DailyIncomeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyIncomeRepository extends JpaRepository<DailyIncomeEntity, String> {
-
-}
