6eafb75ac7ded336b48b0aad49c7c9cf36db83d5
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-18 11:39:21 +0200

    Refactor ParkingAreaGetService.java --> created getDailyIncome method

--- src/main/java/com/project/parkinglot/service/parking_area/ParkingAreaGetService.java
+++ src/main/java/com/project/parkinglot/service/parking_area/ParkingAreaGetService.java
@@ -2,10 +2,14 @@ package com.project.parkinglot.service.parking_area;
 
 import com.project.parkinglot.model.ParkingArea;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public interface ParkingAreaGetService {
 
     ParkingArea getParkingAreaById(final String parkingAreaId);
 
     ParkingArea getParkingAreaByName(final String name);
 
+    BigDecimal getDailyIncome(final LocalDate date, final String parkingAreaId);
 }
