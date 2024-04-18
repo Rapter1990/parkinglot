e91c30e8b589034de7d9536c041aad57b1c43de3
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-18 11:37:58 +0200

    Refactor ParkingAreaEntity.java --> DailyIncome related field

--- src/main/java/com/project/parkinglot/model/entity/ParkingAreaEntity.java
+++ src/main/java/com/project/parkinglot/model/entity/ParkingAreaEntity.java
@@ -1,8 +1,22 @@
 package com.project.parkinglot.model.entity;
 
 import com.project.parkinglot.common.model.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
@@ -63,11 +77,5 @@ public class ParkingAreaEntity extends BaseEntity {
             mappedBy = "parkingAreaEntity"
     )
     private List<ParkEntity> parkEntities;
-
-    @OneToMany(
-            fetch = FetchType.LAZY,
-            mappedBy = "parkingAreaEntity"
-    )
-    private List<DailyIncomeEntity> dailyIncomeEntities;
 
 }
