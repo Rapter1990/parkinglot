a1453230c853fa70c275c5b79153c609793f5bf0
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-04-18 11:42:00 +0200

    Refactor ParkingAreaRepository.java --> created calculateDailyIncome query method

--- src/main/java/com/project/parkinglot/repository/ParkingAreaRepository.java
+++ src/main/java/com/project/parkinglot/repository/ParkingAreaRepository.java
@@ -2,7 +2,11 @@ package com.project.parkinglot.repository;
 
 import com.project.parkinglot.model.entity.ParkingAreaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface ParkingAreaRepository extends JpaRepository<ParkingAreaEntity, String> {
@@ -20,4 +24,14 @@ public interface ParkingAreaRepository extends JpaRepository<ParkingAreaEntity, 
             final String name
     );
 
+    @Query(
+            value = "SELECT SUM(park.TOTAL_COST) FROM parking_area " +
+                    "LEFT JOIN park " +
+                    "ON parking_area.ID = park.PARKING_AREA_ID " +
+                    "WHERE DATE(park.CHECK_OUT) = :date AND parking_area.ID = :parkingAreaId", nativeQuery = true
+    )
+    Optional<BigDecimal> calculateDailyIncome(
+            final @Param("date") LocalDate date,
+            final @Param("parkingAreaId") String parkingAreaId
+    );
 }
