071f83bc0372aaca593d161de05cf29041a5480c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-05-04 20:27:30 +0300

    Add Java Doc for repository interfaces

--- src/main/java/com/project/parkinglot/repository/PriceRepository.java
+++ src/main/java/com/project/parkinglot/repository/PriceRepository.java
@@ -3,6 +3,9 @@ package com.project.parkinglot.repository;
 import com.project.parkinglot.model.entity.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface named {@link PriceRepository} for managing prices.
+ */
 public interface PriceRepository extends JpaRepository<PriceEntity, String> {
 
 }
--- src/main/java/com/project/parkinglot/repository/VehicleRepository.java
+++ src/main/java/com/project/parkinglot/repository/VehicleRepository.java
@@ -5,12 +5,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface named {@link VehicleRepository} for managing vehicles.
+ */
 public interface VehicleRepository extends JpaRepository<VehicleEntity, String> {
 
+    /**
+     * Checks if a vehicle exists by license plate.
+     *
+     * @param licensePlate the license plate of the vehicle
+     * @return true if the vehicle exists, false otherwise
+     */
     Boolean existsByLicensePlate(
             final String licensePlate
     );
 
+    /**
+     * Finds a vehicle by license plate.
+     *
+     * @param licensePlate the license plate of the vehicle
+     * @return the vehicle if found, otherwise empty
+     */
     Optional<VehicleEntity> findByLicensePlate(String licensePlate);
 
 }
