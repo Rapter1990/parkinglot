17be6e303b5d44100928c67c75da6e363d998893
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-05-04 20:51:58 +0300

    Add Java Doc for ParkinglotApplication class

--- src/main/java/com/project/parkinglot/ParkinglotApplication.java
+++ src/main/java/com/project/parkinglot/ParkinglotApplication.java
@@ -3,9 +3,17 @@ package com.project.parkinglot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class named {@link ParkinglotApplication} to start the Parking Lot application.
+ */
 @SpringBootApplication
 public class ParkinglotApplication {
 
+	/**
+	 * Main method to start the Spring Boot application.
+	 *
+	 * @param args the command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ParkinglotApplication.class, args);
 	}
