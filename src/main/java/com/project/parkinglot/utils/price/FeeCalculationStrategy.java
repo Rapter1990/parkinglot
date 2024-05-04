1edd3f6fe3d41e1988df262177e168b96428f51c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-05-04 20:59:07 +0300

    Add Java Doc for Util classes

--- src/main/java/com/project/parkinglot/utils/price/FeeCalculationStrategy.java
+++ src/main/java/com/project/parkinglot/utils/price/FeeCalculationStrategy.java
@@ -2,8 +2,17 @@ package com.project.parkinglot.utils.price;
 
 import java.math.BigDecimal;
 
+/**
+ * Defines a strategy named {@link FeeCalculationStrategy} to calculate the fee.
+ */
 public interface FeeCalculationStrategy {
 
+    /**
+     * Calculates the price based on the given price.
+     *
+     * @param price the base price
+     * @return the calculated price
+     */
     BigDecimal calculatePrice(BigDecimal price);
 
 }
